369e8b94b73b3dc5a4f5e259eaae43810c611da6
Mister Tanatswa <tanatswasingano@gmail.com>
2022-05-08 22:53:23 -0400

    set colors for past present and future

--- script.js
+++ script.js
@@ -21,4 +21,22 @@ $(document).ready(function () {
       $(".time-block").each(function () {
           var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-          
+      // To check the time and add the classes for background indicators
+      if (blockTime < timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }
+    else if (blockTime === timeNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+
+    }
+})
+}    
